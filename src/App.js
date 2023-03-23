0bacd07e26980dc0283494815b4200409fd98db9
Bhawna Gaur <gaurbhawna90@gmail.com>
2023-03-23 23:04:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import {useRef} from "react"
+
 
 function App() {
+  const usernameRef = useRef()
+  const passwordRef = useRef()
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" placeholder='username' ref={usernameRef}></input>
+      <input type="password" placeholder='password' ref={passwordRef}></input>
+      <button>login</button>
     </div>
   );
 }
